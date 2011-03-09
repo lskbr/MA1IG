cbf45fb8c10e529d25182c8446be7ed454cb9831
Nicolas <ndech13@hotmail.com>
2011-03-09 17:12:27 +0100

    Changements design divers

--- apps/backend/modules/language/templates/_flag.php
+++ apps/backend/modules/language/templates/_flag.php
@@ -1,3 +1,5 @@
 <?php
-if($language->getFlag()!=null)
-	echo '<img class="flag" src="/uploads/flags/<?php echo $language->getFlag();?>"/>';
+if($language->getFlag()!=null):
+?>
+<img class="flag" src="/uploads/flags/<?php echo $language->getFlag();?>"/>
+<?php endif; ?>

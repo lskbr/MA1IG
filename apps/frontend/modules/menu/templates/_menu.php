2285ac13bc47b79855414c5f43ea2fd8f1709291
Olivier <jin_7281@hotmail.com>
2011-03-16 22:07:45 +0100

    Menu (2e jet)

--- apps/frontend/modules/menu/templates/_menu.php
+++ apps/frontend/modules/menu/templates/_menu.php
@@ -8,7 +8,7 @@
                 <ul>
                     <?php foreach($pages[$category->getId()] as $p)
                     {
-                        echo '<li><a href="'.$p->getUrl().'">'.$p->getMenuTitle().'</a></li>';
+                        echo '<li><a href="'.$p->getUrl().'"><img src="/images/hierarchie.png" style="vertical-align:0%"/> '.$p->getMenuTitle().'</a></li>';
                     }?>
                 </ul>
             </li>

c2d35c6ee2bc6e9a628a5e1c82d3c23d2221a68d
Laurent5 <laurent_cardon@hotmail.com>
2011-03-16 14:31:29 +0100

    Après pull

--- apps/backend/modules/category/actions/actions.class.php
+++ apps/backend/modules/category/actions/actions.class.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/categoryGeneratorConfiguration.class.php';
-require_once dirname(__FILE__).'/../lib/categoryGeneratorHelper.class.php';
+require_once dirname(__FILE__) . '/../lib/categoryGeneratorConfiguration.class.php';
+require_once dirname(__FILE__) . '/../lib/categoryGeneratorHelper.class.php';
 
 /**
  * category actions.
@@ -11,6 +11,27 @@ require_once dirname(__FILE__).'/../lib/categoryGeneratorHelper.class.php';
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class categoryActions extends autoCategoryActions
-{
+class categoryActions extends autoCategoryActions {
+
+    public function executeListMonter(sfWebRequest $request) {
+
+        $result = Category::monter($this->getRoute()->getObject());
+        $this->printResult($result);
+    }
+    
+    public function executeListDescendre(sfWebRequest $request){
+        $result = Category::descendre($this->getRoute()->getObject());
+        $this->printResult($result);
+    }
+
+    private function printResult($result) {
+        if ($result) {
+            $this->getUser()->setFlash('notice', 'Position dans le menu modifiée');
+        } else {
+            $this->getUser()->setFlash('error', 'Impossible de changer la position dans le menu');
+        }
+
+        $this->redirect('category');
+    }
+
 }

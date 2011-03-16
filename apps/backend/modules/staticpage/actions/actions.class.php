1994008bbae8ee0352cda7b0a3009a7516e85d64
Laurent5 <laurent_cardon@hotmail.com>
2011-03-16 15:50:55 +0100

    Mise à jour du schéma

---
+++ apps/backend/modules/staticpage/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/staticpageGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/staticpageGeneratorHelper.class.php';
+
+/**
+ * staticpage actions.
+ *
+ * @package    grainedevie
+ * @subpackage staticpage
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class staticpageActions extends autoStaticpageActions
+{
+}
---
+++ apps/backend/modules/staticpage/lib/staticpageGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * staticpage module helper.
+ *
+ * @package    grainedevie
+ * @subpackage staticpage
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class staticpageGeneratorHelper extends BaseStaticpageGeneratorHelper
+{
+}

64766f53b46f162d830d64613ef3c0ad02d77a55
Nicolas <ndech13@hotmail.com>
2011-04-24 22:31:34 +0200

    correction bug langue/news

--- apps/frontend/modules/news/actions/actions.class.php
+++ apps/frontend/modules/news/actions/actions.class.php
@@ -17,7 +17,7 @@ class newsActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
-    $this->news=Doctrine_Core::getTable('News')->createQuery('a')->where("a.publication_date < NOW()")->andWhere("a.is_activated=?",true)->orderBy('a.publication_date DESC')->execute();
+    $this->news=Doctrine_Core::getTable('News')->createQuery('a')->leftJoin('a.Language t')->where("a.publication_date < NOW()")->andWhere("a.is_activated=?",true)->andWhere('t.abbreviation= ?',$this->getUser()->getCulture())->orderBy('a.publication_date DESC')->execute();
   }
   public function executeShow(sfWebRequest $request)
   {

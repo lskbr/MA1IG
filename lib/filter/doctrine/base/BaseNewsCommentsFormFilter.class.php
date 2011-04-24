033b956961ca55977766526ecf964c6b3cf476e2
Nicolas <ndech13@hotmail.com>
2011-04-25 01:27:15 +0200

    Commentaires 1

---
+++ lib/filter/doctrine/base/BaseNewsCommentsFormFilter.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * NewsComments filter form base class.
+ *
+ * @package    grainedevie
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BaseNewsCommentsFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'content'    => new sfWidgetFormFilterInput(),
+      'author_id'  => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('sfGuardUser'), 'add_empty' => true)),
+      'news_id'    => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('News'), 'add_empty' => true)),
+      'father_id'  => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+    ));
+
+    $this->setValidators(array(
+      'content'    => new sfValidatorPass(array('required' => false)),
+      'author_id'  => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('sfGuardUser'), 'column' => 'id')),
+      'news_id'    => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('News'), 'column' => 'id')),
+      'father_id'  => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+      'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+    ));
+
+    $this->widgetSchema->setNameFormat('news_comments_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'NewsComments';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'         => 'Number',
+      'content'    => 'Text',
+      'author_id'  => 'ForeignKey',
+      'news_id'    => 'ForeignKey',
+      'father_id'  => 'Number',
+      'created_at' => 'Date',
+      'updated_at' => 'Date',
+    );
+  }
+}

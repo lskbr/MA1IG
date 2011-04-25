05ee82b63da18523b234cb8f3dd383eb78618ef8
Nicolas <ndech13@hotmail.com>
2011-04-25 18:30:23 +0200

    indication si on repond a un message ou pas

--- apps/frontend/modules/news/templates/showSuccess.php
+++ apps/frontend/modules/news/templates/showSuccess.php
@@ -8,14 +8,6 @@ if(config::getInstance()->get('citation'))
 <p class="date_ind">Posté le <?php echo date('d/m/Y',strtotime($news->getPublicationDate())); ?>.<p>
 <div class="news_ind">
 <p><?php echo $news->getContent(ESC_RAW); ?></p>
-<?php
-if(config::getInstance()->get('social_sharing'))
-    include_partial('social_sharing/social_sharing', array(
-        'url_news' => url_for('news_show', $news),
-        'url_title' => $news->getTitle(),
-        'url_description' => $news->getFormatedText(config::getInstance()->get('char_by_news_list'))
-    ))
-?>
 </div>
 <p><?php echo link_to('Retour à la liste des news','news') ?><p>
 

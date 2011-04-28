58930fa11ed2561293c57fdb7377c7b9758f9694
Nicolas <ndech13@hotmail.com>
2011-04-28 22:25:36 +0200

    non affichage galleries vides

--- apps/frontend/modules/gallery/templates/indexSuccess.php
+++ apps/frontend/modules/gallery/templates/indexSuccess.php
@@ -2,13 +2,21 @@
   <h1><?php echo __('Galerie photos')?></h1>
 </div>
 
-<?php foreach ($galerys as $galery): ?>
+<?php 
+foreach ($galerys as $galery):
+	$photo_count=sizeof($galery->getPhoto());
+	if($photo_count>=1):
+?>
     <div class="gallery_thumb">
-      <?php $image_thumb_link=$galery->getPhoto();
+      <?php 
+      $image_thumb_link=$galery->getPhoto();
       $image_thumb_link="/uploads/photo/thumbnail/".$image_thumb_link[0]->getUrl();
-      $photo_count=sizeof($galery->getPhoto())?>
+      ?>
      <a href="<?php echo url_for('gallery/'.$galery->getId()) ?>"> <img class="photo_thumb_padding" src="<?php echo $image_thumb_link ?>"></a>
       <a href="<?php echo url_for('gallery/'.$galery->getId()) ?>"><?php echo $galery->getName() ?></a>
      <p> <?php echo $photo_count.' '.format_number_choice('[1]photo|(1,+Inf]photos',array(),$photo_count); ?></p>
     </div>
-<?php endforeach; ?>
+<?php
+	endif;
+endforeach; 
+?>

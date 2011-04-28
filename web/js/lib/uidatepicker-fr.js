45ca39ba9e1f58e4fa77a18464145a894e58e58b
Laurent5 <laurent_cardon@hotmail.com>
2011-04-28 16:06:49 +0200

    Evoi

---
+++ web/js/lib/uidatepicker-fr.js
@@ -0,0 +1,23 @@
+/* French initialisation for the jQuery UI date picker plugin. */
+/* Written by Keith Wood (kbwood@iprimus.com.au) and Stéphane Nahmani (sholby@sholby.net). */
+
+jQuery(function($){
+	$.datepicker.regional['fr'] = {clearText: 'Effacer', clearStatus: '',
+		closeText: 'Fermer', closeStatus: 'Fermer sans modifier',
+		prevText: '&lt;Préc', prevStatus: 'Voir le mois précédent',
+		nextText: 'Suiv&gt;', nextStatus: 'Voir le mois suivant',
+		currentText: 'Courant', currentStatus: 'Voir le mois courant',
+		monthNames: ['janvier','février','mars','avril','mai','juin',
+		'juillet','août','septembre','octobre','novembre','décembre'],
+		monthNamesShort: ['Jan','Fév','Mar','Avr','Mai','Jun',
+		'Jul','Aoû','Sep','Oct','Nov','Déc'],
+		monthStatus: 'Voir un autre mois', yearStatus: 'Voir un autre année',
+		weekHeader: 'Sm', weekStatus: '',
+		dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+		dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
+		dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
+		dayStatus: 'Utiliser DD comme premier jour de la semaine', dateStatus: 'Choisir le DD, MM d',
+		dateFormat: 'dd/mm/yy', firstDay: 0,
+		initStatus: 'Choisir la date', isRTL: false};
+	$.datepicker.setDefaults($.datepicker.regional['fr']);
+});

3026dde973418695d27550136873772489c20068
Olivier <jin_7281@hotmail.com>
2011-05-02 21:24:48 +0200

    Calcul de l'empreinte ecologique

--- apps/frontend/modules/bilan_carbone/actions/actions.class.php
+++ apps/frontend/modules/bilan_carbone/actions/actions.class.php
@@ -20,6 +20,30 @@ class bilan_carboneActions extends sfActions {
         if ($request->isMethod('post')) {
             $this->form->bind($request->getParameter('bilan_carbone'));
             if ($this->form->isValid()) {
+                $bilan = array(
+                    'nbr_people' => $request->getParameter('nbr_people'),
+                    'fuel' => $request->getParameter('fuel'),
+                    'gas' => $request->getParameter('gas'),
+                    'wood' => $request->getParameter('wood'),
+                    'elec' => $request->getParameter('elec'),
+
+                    'km1' => $request->getParameter('km1'),
+                    'co21' => $request->getParameter('co21'),
+                    'km2' => $request->getParameter('km2'),
+                    'co22' => $request->getParameter('co22'),
+
+                    'nbr_plane' => $request->getParameter('nbr_plane'),
+                    'km_plane' => $request->getParameter('km_plane'),
+                    'train' => $request->getParameter('train'),
+                    'bus' => $request->getParameter('bus'),
+                    'metro' => $request->getParameter('metro'),
+
+                    'computers' => $request->getParameter('computers'),
+                    'books' => $request->getParameter('books'),
+                    'pets' => $request->getParameter('pets'),
+                    'yacht' => $request->getParameter('yacht'),
+                );
+                
                 /* Calcul de l'empreinte écologique */
                 $eco_footprint = '#empreinte écologique#';
                 $nbr_trees = '#nombre d\'arbres#';
@@ -28,8 +52,8 @@ class bilan_carboneActions extends sfActions {
                 $this->getUser()->setAttribute('eco_footprint', $eco_footprint);
                 $this->getUser()->setAttribute('nbr_trees', $nbr_trees);
 
-                $this->getUser()->getAttributeHolder()->remove('eco_footprint');
-                $this->getUser()->getAttributeHolder()->remove('nbr_trees');
+                //$this->getUser()->getAttributeHolder()->remove('eco_footprint');
+                //$this->getUser()->getAttributeHolder()->remove('nbr_trees');
 
                 $this->redirect('bilan_carbone/calcul'); //?'.http_build_query($this->form->getValues()));
             }
@@ -41,12 +65,6 @@ class bilan_carboneActions extends sfActions {
     }
 
     public function executeCalcul(sfWebRequest $request) {
-        //$this->bilan = $this->getRoute()->getObject();
-
-        //$params = $this->bilan->getRequestParameter('nbr_people');
-
-        $this->mlsdkhrg = array(
-            'nbr_people' => $request->getParameter('nbr_people'),
-        );
+        
     }
 }

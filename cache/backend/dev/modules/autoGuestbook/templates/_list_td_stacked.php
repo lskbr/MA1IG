1661028be7f7150c847f8add350e34b69305618e
Nicolas <ndech13@hotmail.com>
2011-05-05 22:41:40 +0200

    Fin du livre d'or

--- cache/backend/dev/modules/autoGuestbook/templates/_list_td_stacked.php
+++
@@ -1,3 +0,0 @@
-<td colspan="4">
-  <?php echo __('%%flag%% - %%content%% - %%created_at%% - %%is_validated%%', array('%%flag%%' => get_partial('guestbook/flag', array('type' => 'list', 'guestbook' => $guestbook)), '%%content%%' => link_to($guestbook->getContent(), 'guestbook_edit', $guestbook), '%%created_at%%' => get_partial('guestbook/created_at', array('type' => 'list', 'guestbook' => $guestbook)), '%%is_validated%%' => get_partial('guestbook/is_validated', array('type' => 'list', 'guestbook' => $guestbook))), 'messages') ?>
-</td>

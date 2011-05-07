cb30323ffaa60861887f956769054fc67a71225f
Olivier <jin_7281@hotmail.com>
2011-05-07 21:38:21 +0200

    Bilan Carbone

--- apps/frontend/modules/bilan_carbone/templates/calculSuccess.php
+++ apps/frontend/modules/bilan_carbone/templates/calculSuccess.php
@@ -12,7 +12,7 @@ slot('title', $page_title); ?>
         <?php echo __('Votre empreinte écologique s\'élève à :').'<br/><span class="bilan_nbr">'.format_number(round($sf_user->getAttribute('eco_footprint'), 2)).'</span> kg équiv. CO<sub>2</sub>'; ?>
     </div>
     <div class="bilan_don">
-        <?php echo __('Pour compenser celle-ci totalement, il faudrait planter :<br/>%nbr_trees% arbres', array('%nbr_trees%' => '<span class="bilan_nbr">'.format_number(ceil($sf_user->getAttribute('nbr_trees'))).'</span>')); ?>
+        <?php echo __('Pour compenser celle-ci totalement, il faudrait planter :').'<br/><span class="bilan_nbr">'.format_number(ceil($sf_user->getAttribute('nbr_trees'))).'</span> '.__('arbres'); ?>
     </div>
     <div class="bilan_don">
         <?php echo __('Aidez-nous à la compenser (au moins en partie) grâce à un don :') ?><br/>

f9a51dd720197eca124e2f643220fbab1063350e
Olivier <jin_7281@hotmail.com>
2011-05-08 19:56:07 +0200

    Frontend Google Analytics

---
+++ apps/frontend/modules/google_analytics/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * google_analytics actions.
+ *
+ * @package    grainedevie
+ * @subpackage google_analytics
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class google_analyticsActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}

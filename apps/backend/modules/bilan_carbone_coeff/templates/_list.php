270cf485bf24d18b9053567dd3e3d05bd43da4ca
Olivier <jin_7281@hotmail.com>
2011-05-08 18:04:34 +0200

    Backend Bilan Carbone + details

---
+++ apps/backend/modules/bilan_carbone_coeff/templates/_list.php
@@ -0,0 +1,36 @@
+<div class="sf_admin_list">
+  <?php if (!$pager->getNbResults()): ?>
+    <p><?php echo __('No result', array(), 'sf_admin') ?></p>
+  <?php else: ?>
+    <table cellspacing="0">
+      <thead>
+        <tr>
+          <?php include_partial('bilan_carbone_coeff/list_th_tabular', array('sort' => $sort)) ?>
+          <th id="sf_admin_list_th_actions"><?php echo __('Actions', array(), 'sf_admin') ?></th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th colspan="4">
+            <?php if ($pager->haveToPaginate()): ?>
+              <?php include_partial('bilan_carbone_coeff/pagination', array('pager' => $pager)) ?>
+            <?php endif; ?>
+
+            <?php echo format_number_choice('[0] no result|[1] 1 result|(1,+Inf] %1% results', array('%1%' => $pager->getNbResults()), $pager->getNbResults(), 'sf_admin') ?>
+            <?php if ($pager->haveToPaginate()): ?>
+              <?php echo __('(page %%page%%/%%nb_pages%%)', array('%%page%%' => $pager->getPage(), '%%nb_pages%%' => $pager->getLastPage()), 'sf_admin') ?>
+            <?php endif; ?>
+          </th>
+        </tr>
+      </tfoot>
+      <tbody>
+        <?php foreach ($pager->getResults() as $i => $bilan_carbone_coeff): $odd = fmod(++$i, 2) ? 'odd' : 'even' ?>
+          <tr class="sf_admin_row <?php echo $odd ?>">
+            <?php include_partial('bilan_carbone_coeff/list_td_tabular', array('bilan_carbone_coeff' => $bilan_carbone_coeff)) ?>
+            <?php include_partial('bilan_carbone_coeff/list_td_actions', array('bilan_carbone_coeff' => $bilan_carbone_coeff, 'helper' => $helper)) ?>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  <?php endif; ?>
+</div>
---
+++ apps/backend/modules/bilan_carbone_coeff/templates/_list_td_actions.php
@@ -0,0 +1,5 @@
+<td>
+  <ul class="sf_admin_td_actions">
+    <?php echo $helper->linkToEdit($bilan_carbone_coeff, array(  'params' =>   array(  ),  'class_suffix' => 'edit',  'label' => 'Edit',)) ?>
+  </ul>
+</td>

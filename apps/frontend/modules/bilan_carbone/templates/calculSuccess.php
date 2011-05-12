3a3df58d36d1988a88bcd89dbc975828e920ad12
Olivier <jin_7281@hotmail.com>
2011-05-12 16:44:24 +0200

    Bilan Carbone

--- apps/frontend/modules/bilan_carbone/templates/calculSuccess.php
+++ apps/frontend/modules/bilan_carbone/templates/calculSuccess.php
@@ -1,7 +1,7 @@
 <?php
 use_helper('Number');
 
-$page_title = __('RÃ©sultat du calcul de votre empreinte Ã©cologique');
+$page_title = __('Résultat du calcul de votre empreinte écologique');
 
 slot('title', $page_title);
 ?>
@@ -10,13 +10,13 @@ slot('title', $page_title);
 
 <div id="bilan_calcul">
     <div id="bilan_result">
-        <?php echo __('Votre empreinte Ã©cologique s\'Ã©lÃ¨ve Ã  :').'<br/><span class="bilan_nbr">'.format_number(round($sf_user->getAttribute('eco_footprint'), 2)).'</span> '.__('kg Ã©quiv. CO<sub>2</sub> par an'); ?>
+        <?php echo __('Votre empreinte écologique s\'élève à :').'<br/><span class="bilan_nbr">'.format_number(round($sf_user->getAttribute('eco_footprint'), 2)).'</span> '.__('kg équiv. CO<sub>2</sub> par an'); ?>
     </div>
     <div class="bilan_don">
         <?php echo __('Pour compenser celle-ci totalement, il faudrait planter :').'<br/><span class="bilan_nbr">'.format_number(ceil($sf_user->getAttribute('nbr_trees'))).'</span> '.__('arbres'); ?>
     </div>
     <div class="bilan_don">
-        <?php echo __('Aidez-nous Ã  la compenser grÃ¢ce Ã  un don (<b>%number%</b>&nbsp;â‚¬/arbres) :', array('%number%' => $cost_tree)) ?><br/>
+        <?php echo __('Aidez-nous à la compenser grâce à un don (%number%&nbsp;€/arbres) :', array('%number%' => $cost_tree)) ?><br/>
         <?php include_component('donenligne', 'show', array('montant' => round($sf_user->getAttribute('montant_don'), 2))); ?>
     </div>
 </div>
